133179145a02b71dee1b55dcde7bf42c332f2b55
lucassherwin <lksherwin@gmail.com>
2020-05-27 15:33:33 -0400

    changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,16 +3,20 @@ class PostsController < ApplicationController
         @posts = Post.all
         render json: @posts
     end
+
     def show
         @post = Post.find_by(id: params[:id])
         render json: @post
     end
+
     def create
         @post = Post.new(post_params)
         @post.save
         render json: @post
     end
+
     private
+    
     def post_params
         params.require(:post).permit(:img_url, :user_id, :created_at, :updated_at)
     end
