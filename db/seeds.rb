62ddcd8926cefd89f94fe2479cd4a65aac835230
stephenverdi95 <stephenverdi152@gmail.com>
2020-05-31 13:30:48 -0400

    commit may 31

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user1 = User.create(name: "User1Name")
-post1 = Post.create(img_url: "fakeurl", user_id: 1)
+user1 = User.create(username: "User1Name")
+post1 = Post.create(img_url: "https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg", user_id: 1)
 caption1 = Caption.create(top_text: "TopText1", bottom_text: "BottomText1", user_id: 1, post_id: 1)
